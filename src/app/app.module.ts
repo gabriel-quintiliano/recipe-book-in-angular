4ae23658194452ef55d7836c13efd6af36ad3811
Gabriel Quintiliano Rosa pereira <gabrielq.pereira@gmail.com>
2024-05-25 08:53:09 -0300

    create basic components regarding pages and layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './components/pages/auth/auth.component';
+import { RecipesComponent } from './components/pages/recipes/recipes.component';
+import { ShoppingListComponent } from './components/pages/shopping-list/shopping-list.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    RecipesComponent,
+    ShoppingListComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
