947f689b12db7099603cb8ecefbdc4178c8a69ce
Gabriel Quintiliano Rosa pereira <gabrielq.pereira@gmail.com>
2024-05-24 22:54:33 -0300

    create route paths and brand name in global environment

--- src/environments/environment.global.ts
+++ src/environments/environment.global.ts
@@ -1,6 +1,18 @@
 // This data will be the same in every environment (dev, prod and others...):
 
-export const globalEnvironment = { } as const;
+export const globalEnvironment = {
+  brandName: $localize`Gabriel's Recipe Book`,
+  routePath: {
+    auth: $localize`:this will be part of a URL - CANNOT contain accents - All lowercase - No spaces:auth`,
+    recipes: $localize`:this will be part of a URL - CANNOT contain accents - All lowercase - No spaces:recipes`,
+    shoppingList: {
+      default: $localize`:this will be part of a URL - CANNOT contain accents - All lowercase - No spaces:shopping-list`,
+      variant1: $localize`:this will be part of a URL - CANNOT contain accents - All lowercase - No spaces:shoppinglist`,
+      variant2: $localize`:this will be part of a URL - CANNOT contain accents - All lowercase - No spaces:shoppingList`,
+    },
+    edit: $localize`:this will be part of a URL - CANNOT contain accents - All lowercase - No spaces:edit`,
+  }
+} as const;
 
 /* This type will make sure all environments have the same keys as globalEnvironment
  * along with default keys which have specific values for that environment. */
