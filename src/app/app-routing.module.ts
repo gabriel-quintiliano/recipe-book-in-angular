a31d3d75fc975bad0e00f897665abc2065d7bb1d
Gabriel Quintiliano Rosa pereira <gabrielq.pereira@gmail.com>
2024-05-25 16:04:25 -0300

    create basic routing setup and tweak typograph styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
+import { environment as env } from 'src/environments/environment';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipesComponent } from './components/pages/recipes/recipes.component';
+import { AuthComponent } from './components/pages/auth/auth.component';
+import { ShoppingListComponent } from './components/pages/shopping-list/shopping-list.component';
 
-const routes: Routes = [];
+// it all uses values from `environment` because of i18n of route paths here and in template links
+const routes: Routes = [
+  { path: "", redirectTo: env.routePath.recipes, pathMatch: "full" },
+  { path: env.routePath.recipes, component: RecipesComponent },
+  { path: env.routePath.auth, component: AuthComponent },
+  { path: env.routePath.shoppingList.default, component: ShoppingListComponent },
+  { path: env.routePath.shoppingList.variant1, redirectTo: env.routePath.shoppingList.default },
+  { path: env.routePath.shoppingList.variant2, redirectTo: env.routePath.shoppingList.default },
+  { path: "**", redirectTo: env.routePath.recipes }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
